85250d2dae474abe1580a3382d72d0bf058899c5
Jefferson <jeffalexandrino07@gmail.com>
2024-03-27 17:10:18 -0300

    aa

---
+++ atividades/atv2/Jogador.java
@@ -0,0 +1,39 @@
+public class Jogador extends GeradorJogador {
+    public String nome;
+    public String sobrenome;
+    public short idade;
+    public String posicao;
+    public String clubes;
+
+    public Jogador (String nome, String sobrenome, short idade, String posicao, String clube) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.idade = idade;
+        this.posicao = posicao;
+        this.clubes = clube;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public short getIdade() {
+        return idade;
+    }
+
+    public String getPosicao() {
+        return posicao;
+    }
+
+    public String getClube() {
+        return clube;
+    }
+
+    public Jogador(){
+        Jogador = new Jogador();
+    }
+}
