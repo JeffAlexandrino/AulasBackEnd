851f78de36d5d4c9b2acb35ee9c56cc47858894c
Jefferson <jeffalexandrino07@gmail.com>
2024-04-03 16:19:35 -0300

    atv4

---
+++ atividades/atv4/src/main/controllers/JogadorControle.java
@@ -0,0 +1,17 @@
+package com.example.demo.controllers;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.*;
+
+@RestController
+public class JogadorControle {
+    @GetMapping("/GerarJogador")
+    public String GerarJogador() {
+        return JogadorGerador.NovoJogador().getDescricao();
+    }
+
+    @GetMapping("/GerarJogadorJson")
+    public Jogador GerarJogadorJson() {
+        return JogadorGerador.NovoJogador();
+    }
+}
